07e263694e88ec5840e3e6f9485160b973b7f2ce
Rahul <rahulreghunath11@gmail.com>
2021-07-16 21:33:12 +0530

    add image compression

--- bot.py
+++ bot.py
@@ -1,19 +1,20 @@
-import os
 import logging
-from datetime import datetime, timedelta
+import os
 from uuid import uuid4
+from PIL import Image
+from io import BytesIO
 
 import requests
 from dotenv import load_dotenv
-from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update, ParseMode, TelegramError
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update, TelegramError
 from telegram.ext import (
     Updater,
     CommandHandler,
     MessageHandler,
     Filters,
     ConversationHandler,
-    CallbackContext, CallbackQueryHandler,
-)
+    CallbackContext, )
+
 from main import create_qr_code, remove_file
 
 # Enable logging
@@ -27,24 +28,34 @@ load_dotenv('.env')
 
 API_KEY = os.getenv('UTIL_API_KEY')
 
-GENDER, PHOTO, LOCATION, BIO, SELECT_TYPE, PIN, DISTRICT_RESULT, PIN_RESULT, RESTART, QR_CODE_TEXT_INPUT, PIN_INPUT = range(
-    11)
+SELECT_TYPE, RESTART, COMPRESSION_RATE, QR_CODE_TEXT_INPUT, COMPRESSION_IMAGE, PIN_INPUT, IMAGE_TO_COMPRESS = range(7)
 
 
 # Conversation Start
 def start(update: Update, _: CallbackContext) -> int:
-    reply_keyboard = [['/qrcode']]
-    if os.getenv('API_KEY') == 'production':
-        requests.get('https://api.countapi.xyz/hit/namespace/rahulreghunathmannady')
+    reply_keyboard = [['/qrcode', '/compress']]
     user = update.message.from_user
     logger.info("User %s Started the conversation.", user.first_name)
     update.message.reply_text(
         'Hi☺️ \nMy name is blablabla.\n'
-        '/qrcode to create a custom qr code',
+        '/qrcode to create a custom qr code\n'
+        '/compress to compress image',
         reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True),
     )
 
     return SELECT_TYPE
+
+
+def qr_code_text_dialogue(update: Update, _: CallbackContext) -> int:
+    """
+    Select the input option
+    """
+
+    update.message.reply_text(
+        'Enter your text',
+        reply_markup=ReplyKeyboardRemove(),
+    )
+    return QR_CODE_TEXT_INPUT
 
 
 def qr_code_response(update: Update, _: CallbackContext) -> int:
@@ -64,16 +75,57 @@ def qr_code_response(update: Update, _: CallbackContext) -> int:
     return SELECT_TYPE
 
 
-def qr_code_text_dialogue(update: Update, _: CallbackContext) -> int:
+def image_compression_get_image(update: Update, _: CallbackContext) -> int:
+    """
+    Read the district if success ask for date
+    """
+    file = update.message.photo[-1].get_file()
+    image = Image.open(requests.get(file.file_path, stream=True).raw)
+    compressed = BytesIO()
+    compressed.name = f'compressed_{file.file_path.split("/")[-1]}'
+    global IMAGE_TO_COMPRESS
+    IMAGE_TO_COMPRESS = {'image': image, 'compressed': compressed}
+
+    update.message.reply_text(
+        'Enter compression rate out of 100',
+        reply_markup=ReplyKeyboardRemove(),
+    )
+    return COMPRESSION_RATE
+
+
+def image_compression_get_compression_rate(update: Update, _: CallbackContext) -> int:
+    """
+    Read the district if success ask for date
+    """
+    file = update.message.photo[-1].get_file()
+    global IMAGE_TO_COMPRESS
+    IMAGE_TO_COMPRESS = Image.open(requests.get(file.file_path, stream=True).raw)
+
+    return SELECT_TYPE
+
+
+def compress_image_dialogue(update: Update, _: CallbackContext) -> int:
     """
     Select the input option
     """
 
     update.message.reply_text(
-        'Enter your text',
+        'Send image to compress',
         reply_markup=ReplyKeyboardRemove(),
     )
-    return QR_CODE_TEXT_INPUT
+    return COMPRESSION_IMAGE
+
+
+def compressed_image_response(update: Update, _: CallbackContext) -> int:
+    """
+    Select the input option
+    """
+    IMAGE_TO_COMPRESS['image'].save(IMAGE_TO_COMPRESS['compressed'], optimize=True, quality=int(update.message.text))
+    IMAGE_TO_COMPRESS['compressed'].seek(0)
+    update.message.reply_photo(
+        photo=IMAGE_TO_COMPRESS['compressed']
+    )
+    return SELECT_TYPE
 
 
 def cancel(update: Update, _: CallbackContext) -> int:
@@ -100,11 +152,18 @@ def main() -> None:
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler('start', start), CommandHandler('restart', start)],
         states={
-            SELECT_TYPE: [MessageHandler(
-                Filters.regex('^(/qrcode)$'),
-                qr_code_text_dialogue
-            )],
-            QR_CODE_TEXT_INPUT: [MessageHandler(Filters.all, qr_code_response)],
+            SELECT_TYPE: [
+                MessageHandler(
+                    Filters.regex('^(/qrcode)$'),
+                    qr_code_text_dialogue
+                ),
+                MessageHandler(
+                    Filters.regex('^(/compress)$'),
+                    compress_image_dialogue
+                )],
+            QR_CODE_TEXT_INPUT: [MessageHandler(Filters.text, qr_code_response)],
+            COMPRESSION_IMAGE: [MessageHandler(Filters.photo, image_compression_get_image)],
+            COMPRESSION_RATE: [MessageHandler(Filters.regex('^[1-9][0-9]?$|^100$'), compressed_image_response)],
         },
         allow_reentry=True,
         fallbacks=[CommandHandler('stop', cancel)],
